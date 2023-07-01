f87f16268c4d9c76d44a85703a5b8e637b285a1b
Vibhu Gupta <vik6026717961@gmail.com>
2023-07-01 11:58:31 +0530

    new commit

---
+++ Strings/String_value.c
@@ -0,0 +1,12 @@
+#include <string.h>
+int main (){
+    char new[3] = {'b', 'v', 'n' };
+
+    for (int  i = 0; i < 3; i++)
+    {
+        printf("%c \n ",new);
+    }
+    
+    
+    return 0;
+}
